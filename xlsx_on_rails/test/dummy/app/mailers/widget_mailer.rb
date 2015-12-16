7f771672fffd1b4b267220d1dd95a6c9770d20ec
Alex Reyes <alejandroereyes@gmail.com>
2015-12-16 08:16:33 -0600

    added test and prod for widget mailer

---
+++ xlsx_on_rails/test/dummy/app/mailers/widget_mailer.rb
@@ -0,0 +1,18 @@
+class WidgetMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.widget_mailer.to_xlsx.subject
+  #
+  def to_xlsx(widgets)
+    @greeting = "Hi"
+
+    @widgets = widgets
+    xlsx = render_to_string 'widgets/index.xlsx.axlsx'
+    attachments["widgets.xlsx"] = {mime_type: Mime::XLSX, content: xlsx}
+
+    mail to: "to@example.org"
+  end
+end
---
+++ xlsx_on_rails/test/xlsx_mailer_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class XlsxMailerTest < ActionMailer::TestCase
+  test "widgets" do
+    mail = WidgetMailer.to_xlsx Widget.all
+    assert_equal "To xlsx", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+    assert 1, mail.attachments.length
+    assert_equal Mime::XLSX.to_s+'; charset=UTF-8', mail.attachments.first.content_type
+    assert_spreadsheet mail.attachments.first.body, 'attachment body'#, 'Name 0'
+  end
+end
